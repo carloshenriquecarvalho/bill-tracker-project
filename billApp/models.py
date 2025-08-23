0498416b07333e5f294c4c0088e75516315c034f
carlos <misterchaty@outlook.com>
2025-08-22 21:25:51 -0300

    Create Models for Enterprise and Bill

--- billApp/models.py
+++ billApp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Enterprise(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=45)
+    cnpj = models.CharField(max_length=18, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Bill(models.Model):
+    enterprise = models.ForeignKey(
+        Enterprise,
+        on_delete=models.CASCADE,
+        related_name='bills'
+    )
+    overdue_date = models.DateTimeField()
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+    bill_file = models.FileField(upload_to='bills/')
+    notification_date = models.DateTimeField()
+    STATUS_CHOICES = [
+        ("Pending", "Pending"),
+        ("Paid", "Paid"),
+        ("Overdue", "Overdue"),
+    ]
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="Pending")
+
+    def __str__(self) -> str:
+        return f"O boleto pertencente a {self.enterprise.name}, no valor de R${self.value}"
